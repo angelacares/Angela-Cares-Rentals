3a9912e1ebee800e7b2581cdecf6bc80cd5f1db3
angelacares <ade2014@gmail.com>
2025-07-07 01:41:02 +1000

    Update db.py

--- db.py
+++ db.py
@@ -1,39 +1,17 @@
-import sqlite3
-import time
+import json
+import os
 
-DB = 'listings.db'
+DB_FILE = 'listings.json'
 
 def init_db():
-    conn = sqlite3.connect(DB)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS listings (
-                    id TEXT PRIMARY KEY,
-                    title TEXT,
-                    address TEXT,
-                    price TEXT,
-                    link TEXT,
-                    image TEXT,
-                    timestamp INTEGER
-                )''')
-    conn.commit()
-    conn.close()
+    if not os.path.exists(DB_FILE):
+        with open(DB_FILE, 'w') as f:
+            json.dump([], f)
 
 def save_listings(listings):
-    conn = sqlite3.connect(DB)
-    c = conn.cursor()
-    new_ids = set()
+    with open(DB_FILE, 'w') as f:
+        json.dump(listings, f)
 
-    for l in listings:
-        c.execute("REPLACE INTO listings VALUES (?, ?, ?, ?, ?, ?, ?)", (
-            l['id'], l['title'], l['address'], l['price'], l['link'], l['image'], int(time.time())
-        ))
-        new_ids.add(l['id'])
-
-    c.execute("SELECT id FROM listings")
-    all_ids = {row[0] for row in c.fetchall()}
-    removed = all_ids - new_ids
-    for rid in removed:
-        c.execute("DELETE FROM listings WHERE id = ?", (rid,))
-
-    conn.commit()
-    conn.close()
+def get_listings():
+    with open(DB_FILE, 'r') as f:
+        return json.load(f)
